33de01391f88350f82c3bc6df0be336774156bc1
gesongbai <gesongbai@outlook.com>
2021-04-22 13:24:12 +0800

    路由文件更改

--- src/router/index.js
+++ src/router/index.js
@@ -32,17 +32,6 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
-
-  {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
-  },
-  {
     path: '/',
     component: Layout,
     redirect: '/dashboard',
@@ -53,6 +42,16 @@ export const constantRoutes = [
       meta: { title: '首页', icon: 'dashboard', noCache: true,
         affix: true }
     }]
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
   },
   {
     path: '/permisson',
